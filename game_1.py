f23923f064d3d1cb211e3b3cde13fa7060a78f64
vladislav602 <kulycky@gmail.com>
2024-12-10 17:27:21 +0200

    fere

---
+++ game_1.py
@@ -0,0 +1,11 @@
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__()
+        self.land = Mapmanager()
+        base.camLens.setFor(90)
+
+game = Game()
+geme.run()
