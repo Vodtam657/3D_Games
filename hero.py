85395490c9704132d47a2f99194cece9b3e9a1a9
Vodtam657 <151962263+Vodtam657@users.noreply.github.com>
2024-12-19 19:48:12 +0200

    fsvfs

--- hero.py
+++ hero.py
@@ -87,6 +87,88 @@ class Hero():
                 return (0, -1)
 
 
+    def forward(self):
+        angle = (self.hero.getH()) % 360
+
+    def back(self):
+        angle = (self.hero.getH()+180) % 360
+
+    def left(self):
+        angle = (self.hero.getH()+90) % 360
+
+    def right(self):
+        angle = (self.hero.getH()+270) % 360
+
+    def changeMode(self):
+        if self.mode:
+            self.mode = False
+        else:
+            self.mode = True
+    def try_move(self, angle):
+        pos = self.look_at(angle)
+        if self.land.isEmpty(pos):
+            pos = self.land.findHighestEmpty(pos):
+            self.hero.setPos(pos)
+        else:
+            pos = pos[0], pos[1], pos[2] + 1
+            if self.land.isEmpty(pos):
+                self.hero.setPos(pos)
+
+    def up(self):
+        if self.mode:
+            self.hero.setZ(self.hero.getZ() + 1)
+
+    def down(self):
+        self.hero.getH() % 360
+
+
+    def build(self):
+        angle = self.hero.getH() % 360
+        pos = self.look_at(angle)
+        if self.mode:
+            self.land.addBlock(pos)
+        else:
+            self.land.buildBlockFrom(pos)
+
+    def destroy(self):
+        angle = self.hero.getH() % 360
+        pos = self.look_at(angle)
+        if self.mode:
+            self.land.delBlock(pos)
+        else:
+            self.land.buildBlockFrom(pos)
+
+    def accept_events(self):
+        base.accept(key_turn_left, self.turn_left)
+        base.accept(key_turn_left + "-repeat", self.turn_left)
+        base.accept(key_turn_right, self.turn_right)
+        base.accept(key_turn_right + "-repeat", self.turn_right)
+
+
+        base.accept(key_forward, self.forward)
+        base.accept(key_forward + "-repeat", self.forward())
+        base.accept(key_back, self.back)
+        base.accept(key_back + "-repeat", self.back)
+        base.accept(key_turn_left, self.left)
+        base.accept(key_turn_left + "-repeat", self.left)
+        base.accept(key_turn_right, self.right)
+        base.accept(key_turn_right + "-repeat", self.right)
+
+
+        base.accept(key_swich_camera, self.changeView)
+        base.accept(key_swich_mode, self.changeMode)
+
+        base.accept(key_up, self.up)
+        base.accept(key_up + "-repeat", self.up)
+        base.accept(key_down, self.down)
+        base.accept(key_down + "-repeat", self.down)
+
+        base.accept(key_build, self.build)
+        base.accept(key_destroy, self.destroy)
+
+
+
+
 
 
 
